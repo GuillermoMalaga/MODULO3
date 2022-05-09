13c0bbc3dd69151ba119ba862848fdbfb14d52f0
Guillermo Malaga <gmg.proyectos.2013@gmail.com>
2022-05-09 09:50:51 -0400

    inicio2

--- M03C01/js/clase_01.js
+++ M03C01/js/clase_01.js
@@ -1,1 +1,40 @@
 //archivo de la clase 01
+var autoAlexis={
+    motor:"2.2",
+    puertas:5,
+    combustible:"diesel",
+    sonido:function(){
+        console.log("BRUM BRUM,");
+    }
+}
+
+console.log(auto.combustible);
+auto.sonido();
+var autoSandra={
+    modelo:"tp",
+    marca:"pegeut",
+    airbag:2,
+    combustible:"bencina",
+    sonido:function(){
+        console.log("brum brum");
+    }
+}
+var autoGuillermo={
+    modelo:"tp",
+    marca:"pegeut",
+    airbag:2,
+    combustible:"bencina",
+    sonido:function(){
+        console.log("brum brum");
+    }
+}
+
+var autoElectrico={
+    modelo:"tpzzelectrivco1",
+    marca:"tesla",
+    airbag:2,
+    combustible:"electricidad",
+    sonido:function(){
+        console.log("fiz fiz");
+    }
+}
